cedf3c43beca38b338265368a42ca1a82295a304
LuisKrotz <luis.krotz@gmail.com>
2021-08-17 10:34:13 -0300

    SLIDER: test

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,15 +1,19 @@
 <template>
-    <video :class="classes" playsinline autoplay loop muted controls :poster="storage + poster">
-        <source :src="storage + src" type="video/mp4">
-      </video>
+    <video :class="classes" playsinline autoplay loop muted :poster="poster">
+        <source :src="video" type="video/mp4">
+    </video>
 </template>
 
 <script>
+const placeholder = '.mp4.jpg-thumb.jpg', extension = '.mp4'
+
 export default {
   name: 'Video',
     data() {
         return {
-            storage: this.$store.getters.getStorage
+            storage: this.$store.getters.getStorage,
+            poster: '',
+            video: ''
         }
     },
   props: {
@@ -23,11 +27,28 @@ export default {
             default: '',
             required: true
         },
-        poster: {
+        label: {
             type: String,
             default: '',
+            required: false
+        },
+        width: {
+            type: Number,
             required: true
+        },
+        height: {
+            type: Number,
+            required: true
+        },
+        canExpand: {
+            type: Boolean,
+            default: false,
+            required: false
         }
-  }
+    },
+    created() {
+        this.poster = this.storage + this.src + placeholder;
+        this.video = this.storage + this.src + extension;
+    }
 }
 </script>
--- src/views/portfolio/Aboutmarco.vue
+++ src/views/portfolio/Aboutmarco.vue
@@ -26,63 +26,83 @@
     </section>
 
     <section class="project-extra">
-        <ul class="project-extra-scroll">
-          <li class="project-extra-item">
-            <video playsinline autoplay loop muted controls>
-              <source :src="storage + 'aboutmarco/home/aboutmarco.com-mobile-mobile.mp4'" type="video/mp4">
-            </video>
-            <label>aboutmarco.com - mobile tour</label>
-            <button>Tap to open</button>
-          </li>
-          <li class="project-extra-item">
+      <carousel items-to-show="5.5" :wrapAround="true">
+        <slide key="1">
+          <div class="project-extra-item">
+              <Video
+                src="aboutmarco/home/aboutmarco.com-mobile-mobile"
+                :width="830"
+                :height="1712"
+                :canExpand="true"
+                label="aboutmarco.com - mobile tour"/>
+          </div>
+        </slide>
+        <slide key="2">
+          <div class="project-extra-item">
               <Image
                 src="aboutmarco/pages/aboutmarco.com-pages--melissa-insider-2020-12-15"
                 :width="800"
                 :height="1200"
                 :canExpand="true"
                 label="aboutmarco.com - melissa insider"/>
-          </li>
-          <li class="project-extra-item">
+          </div>
+        </slide>
+        <slide key="3">
+          <div class="project-extra-item">
             <Image
               src="aboutmarco/pages/aboutmarco.com-pages--melissa-2020-12-15"
               :width="800"
               :height="1200"
               :canExpand="true"
               label="aboutmarco.com - melissa"/>
-          </li>
-          <li class="project-extra-item">
+          </div>
+        </slide>
+        <slide key="4">
+          <div class="project-extra-item">
             <Image
               src="aboutmarco/pages/aboutmarco.com-pages--coza-2020-12-15"
               :width="800"
               :height="1200"
               :canExpand="true"
               label="aboutmarco.com - coza"/>
-          </li>
-          <li class="project-extra-item">
+          </div>
+        </slide>
+        <slide key="5">
+          <div class="project-extra-item">
             <Image
               src="aboutmarco/pages/aboutmarco.com-pages--brazilian-leather-2020-12-15"
               :width="800"
               :height="1200"
               :canExpand="true"
               label="aboutmarco.com - brazilian leather"/>
-          </li>
-          <li class="project-extra-item">
+          </div>
+        </slide>
+        <slide key="6">
+          <div class="project-extra-item">
             <Image
               src="aboutmarco/about/aboutmarco.com-me-2020-12-15-23_29_35"
               :width="800"
               :height="1200"
               :canExpand="true"
               label="aboutmarco.com - me"/>
-          </li>
-          <li class="project-extra-item">
+          </div>
+        </slide>
+        <slide key="7">
+          <div class="project-extra-item">
             <Image
               src="aboutmarco/about/aboutmarco.com-archive-2020-12-15-23_31_53"
               :width="800"
               :height="1200"
               :canExpand="true"
               label="aboutmarco.com - archive"/>
-          </li>
-        </ul>
+          </div>
+        </slide>
+
+        <template #addons>
+          <navigation />
+          <pagination />
+        </template>
+      </carousel>
     </section>
     <router-view/>
 
@@ -141,7 +161,11 @@
 </template>
 
 <script>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+
 import Image from'../../components/Image';
+import Video from'../../components/Video';
 
 export default {
   data() {
@@ -150,7 +174,12 @@ export default {
     }
   },
   components: {
-    Image
+    Image,
+    Video,
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation
   },
   methods: {
     openModal() {
