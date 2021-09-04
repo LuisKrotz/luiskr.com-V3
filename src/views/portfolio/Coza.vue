2ee2e650c56f9160eb5d86ad947dc43552593eb5
LuisKrotz <luis.krotz@gmail.com>
2021-09-04 16:47:56 -0300

    COZA: add project

--- src/views/portfolio/Coza.vue
+++ src/views/portfolio/Coza.vue
@@ -1,21 +1,192 @@
 <template>
-  <div class="about">
-    <h1>Coza</h1>
-    <router-view/>
-  </div>
-  <div>INTERNAL FOOTER
-    <ul>
-      <li><router-link to="/portfolio/metcha">metcha</router-link></li>
-      <li><router-link to="/portfolio/transa">transa</router-link></li>
-      <li><router-link to="/portfolio/aboutmarco">aboutmarco</router-link></li>
-      <li><router-link to="/portfolio/melissa">melissa</router-link></li>
-      <li><router-link to="/portfolio/minimelissa">minimelissa</router-link></li>
-      <li><router-link to="/portfolio/mor">mor</router-link></li>
-      <li><router-link to="/portfolio/brazilian-leather">brazilian leather</router-link></li>
-      <li><router-link to="/portfolio/vibra">vibra</router-link></li>
-      <li><router-link to="/portfolio/genesysinf-sageweb">genesysinf</router-link></li>
-    </ul>
+  <div :class="modal.class">
+    <div id="#main" class="project modal-below" :style="'transform: translateY(-' + modal.transform + 'px);'">
+      <h1 class="project-title">Coza</h1>
+      <div class="project-main">
+          <Media
+            classes="project-main-item"
+            src="coza/feed+shop/coza.com.br-feed-2019-tour"
+            :width="1943"
+            :height="976"
+            :isVideo="true"
+            label="coza.com.br - homepage full tour"/>
+      </div>
 
-    <div>Contact Form | Linkedin | Github | Instagram </div>
+      <section class="project-description">
+        <h2 class="project-description-text">
+          Coza's website was developed at transa.
+        </h2>
+        <p class="project-description-text">
+          It mainly uses Vanilla JS, Less and Laravel (PHP). The goaf of the project was to create a fast, beautifull and pixel perfect experience that reflect's the new brading made by Transa for Coza.
+        </p>
+        <p class="project-description-text">
+          Coza's main page is a feed of content created for the brand, with articles, galleries, photos and links to the online store.
+        </p>
+        <p class="project-description-text">
+          The project also have a store front with a list of all the products listed on the online store, feeded by Ajax from the the back-end with the content updated constantly from the Brinox store.
+        </p>
+      </section>
+
+      <section class="project-extra">
+        <carousel :wrapAround="true" :breakpoints="{180: {itemsToShow: 1, snapAlign: 'start'}, 768: {itemsToShow: 5.5, snapAlign: 'center'}}">
+          <slide key="1">
+            <div class="project-extra-item">
+                <Media
+                  src="coza/feed+shop/coza.com.br-31-03-19-1-mobile"
+                  :width="834"
+                  :height="1728"
+                  :canExpand="true"
+                  :isVideo="true"
+                  label="coza.com.br - mobile full tour"/>
+            </div>
+          </slide>
+          <slide key="2">
+            <div class="project-extra-item">
+                <Media
+                  src="coza/feed+shop/coza.com.br-feed-2019-full-page"
+                  :width="1920"
+                  :height="22418"
+                  :canExpand="true"
+                  label="coza.com.br - mobile full tour"/>
+            </div>
+          </slide>
+          <slide key="3">
+            <div class="project-extra-item">
+                <Media
+                  src="coza/feed+shop/coza.com.br-en-2020-12-15-18-54"
+                  :width="1920"
+                  :height="4861"
+                  :canExpand="true"
+                  label="coza.com.br - mobile full tour"/>
+            </div>
+          </slide>
+          <slide key="4">
+            <div class="project-extra-item">
+                <Media
+                  src="coza/articles+galleries+photo/coza-article--organização-coza-com-as-dicas-da-maysa"
+                  :width="1920"
+                  :height="2423"
+                  :canExpand="true"
+                  label="coza.com.br - mobile full tour"/>
+            </div>
+          </slide>
+          <slide key="5">
+            <div class="project-extra-item">
+                <Media
+                  src="coza/articles+galleries+photo/coza-article--image-gallery-a-cada-instante-da-rotina-mais-coza+pantone"
+                  :width="1920"
+                  :height="2249"
+                  :canExpand="true"
+                  label="coza.com.br - mobile full tour"/>
+            </div>
+          </slide>
+          <slide key="6">
+            <div class="project-extra-item">
+                <Media
+                  src="coza/articles+galleries+photo/coza-article--seu-banheiro-cheio-de-beleza"
+                  :width="1920"
+                  :height="2414"
+                  :canExpand="true"
+                  label="coza.com.br - mobile full tour"/>
+            </div>
+          </slide>
+
+          <template #addons>
+            <navigation />
+            <pagination />
+          </template>
+        </carousel>
+      </section>
+
+
+      <section class="project-description">
+        <p class="project-description-text">
+          And a Where to find page, listing all the locations where the Brand cand be found, using the Google Maps API.
+        </p>
+      </section>
+
+
+      <section class="project-extra">
+        <carousel :wrapAround="true" :breakpoints="{180: {itemsToShow: 1, snapAlign: 'start'}, 768: {itemsToShow: 1.75, snapAlign: 'center'}}">
+          <slide key="1">
+            <div class="project-extra-item landscape">
+                <Media
+                  src="coza/where-to-find/coza-onde-encontrar-tour"
+                  :width="1947"
+                  :height="976"
+                  :canExpand="true"
+                  :isVideo="true"
+                  label="coza.com.br - mobile full tour"/>
+            </div>
+          </slide>
+          <slide key="2">
+            <div class="project-extra-item">
+                <Media
+                  src="coza/where-to-find/coza.com.br-onde-encontrar-31-03-19-1-mobile"
+                  :width="834"
+                  :height="1728"
+                  :canExpand="true"
+                  :isVideo="true"
+                  label="coza.com.br - mobile full tour"/>
+            </div>
+          </slide>
+
+          <template #addons>
+            <navigation />
+            <pagination />
+          </template>
+        </carousel>
+      </section>
+  
+      <router-view/>
+
+
+      <Related />
+    </div>
+
+    <aside id="#modal" class="modal-above" v-if="modal.open">
+      <MediaExpanded
+        :source="modal.media.source"
+        :thumb="modal.media.thumb"
+        :alt="modal.media.alt"
+        :width="modal.media.width"
+        :height="modal.media.height"
+        :isVideo="modal.media.isVideo"/>
+    </aside>
   </div>
 </template>
+
+<script>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+
+import Media from'../../components/Media';
+import MediaExpanded from'../../components/MediaExpanded';
+import Related from'../../components/portfolio/Related';
+
+export default {
+  data() {
+    return {
+      modal: this.$store.getters.getModal
+    }
+  },
+  components: {
+    Media,
+    MediaExpanded,
+    Related,
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation
+  },
+  mounted() {
+    window.scrollTo(0,0);
+  },
+  name: 'Coza',
+}
+</script>
+
+<style lang="scss">
+@import '../../sass/projects';
+@import '../../sass/modal';
+</style>
