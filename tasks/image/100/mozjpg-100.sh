84c96cbdb03bff41333409ab4ebc2a03877b2368
LuisKrotz <luis.krotz@gmail.com>
2022-01-02 18:20:34 -0300

    UPDATE V2: fix task script rename

--- tasks/image/100/mozjpg-100.sh
+++ tasks/image/100/mozjpg-100.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 FOLDER=.
-find "$FOLDER" -name '*.png' -exec sh -c 'convert "$0" pnm:- | cjpeg -quality 100 > "${0%%}.jpg"' {} \;
+find "$FOLDER" -name '*.png' -exec sh -c 'convert "$0" pnm:- | cjpeg -quality 100 > "${0%%}-mozjpg-uncompressed.jpg"' {} \;
 exit;
