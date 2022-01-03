a4a7e035a7ba95c45248a5d45ebc5c9225e017fe
LuisKrotz <luis.krotz@gmail.com>
2022-01-03 13:06:50 -0300

    UPDATE V2: create page for nathalia bond clinica de desenvolvimento, added images - todo > text

---
+++ src/views/portfolio/ClinicaDeDesenvolvimentoNathaliaBond.vue
@@ -0,0 +1,489 @@
+<template>
+  <article>
+    <div id="#main" class="project modal-below" :style="'transform: translateY(-' + modal.transform + 'px);'">
+      <h2 class="internal-title">Nathalia Bond<br>Cliníca de Desenvolvimento</h2>
+      <div class="internal-main">
+          <Media
+            classes="internal-main-item"
+            src="nathaliabond/home/nathaliabond.com.br-tour"
+            :width="3836"
+            :height="2056"
+            :isVideo="true"
+            label="Nathalia Boond - Clínica de desenvolvimento: homepage / tour"/>
+      </div>
+
+      <section>
+        <div class="internal-description">
+          <h3 class="internal-description-text">
+
+          </h3>
+          <p class="internal-description-text">
+
+          </p>
+        </div>
+        <div class="internal-extra">
+          <div class="internal-extra-scroll">
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/home/nathaliabond-br-home-desktop"
+                  :width="1920"
+                  :height="8481"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/home/nathaliabond-br-home-mobile"
+                  :width="414"
+                  :height="7763"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section>
+        <div class="internal-description">
+          <h3 class="internal-description-text">
+
+          </h3>
+          <p class="internal-description-text">
+
+          </p>
+        </div>
+        <div class="internal-extra">
+          <div class="internal-extra-scroll">
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/especialidades/nathaliabond.com.br-nossas-especialidades"
+                  :width="3836"
+                  :height="2056"
+                  :canExpand="true"
+                  :isVideo="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/especialidades/nathaliabond-br-nossas-especialidades-desktop"
+                  :width="1920"
+                  :height="8547"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/especialidades/nathaliabond-br-nossas-especialidades-fonoaudiologia-desktop"
+                  :width="1920"
+                  :height="2796"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/especialidades/nathaliabond-br-nossas-especialidades-fonoaudiologia-mobile"
+                  :width="414"
+                  :height="3140"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/especialidades/nathaliabond-br-nossas-especialidades-psicologia-infantil-desktop"
+                  :width="1920"
+                  :height="2846"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/especialidades/nathaliabond-br-nossas-especialidades-psicomotria-desktop"
+                  :width="1920"
+                  :height="2627"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/especialidades/nathaliabond-br-nossas-especialidades-psicopedagogia-desktop"
+                  :width="1920"
+                  :height="2888"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/especialidades/nathaliabond-br-nossas-especialidades-terapia-ocupacional-desktop"
+                  :width="1920"
+                  :height="2218"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section>
+        <div class="internal-description">
+          <h3 class="internal-description-text">
+
+          </h3>
+          <p class="internal-description-text">
+
+          </p>
+        </div>
+
+
+        <div class="internal-extra">
+          <div class="internal-extra-scroll">
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond.com.br-equipe"
+                  :width="3836"
+                  :height="2056"
+                  :canExpand="true"
+                  :isVideo="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond-br-nossa-equipe-desktop"
+                  :width="1920"
+                  :height="5905"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond-br-nossa-equipe-mobile"
+                  :width="414"
+                  :height="5440"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond-br-nossa-equipe-nathalia-desktop"
+                  :width="1920"
+                  :height="1621"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond-br-nossa-equipe-camila-nobre-desktop"
+                  :width="1920"
+                  :height="1795"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond-br-nossa-equipe-camila-teixeira-desktop"
+                  :width="1920"
+                  :height="1796"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond-br-nossa-equipe-ellen-desktop"
+                  :width="1920"
+                  :height="1795"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond-br-nossa-equipe-mariel-desktop"
+                  :width="1920"
+                  :height="1621"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond-br-nossa-equipe-melissa-desktop"
+                  :width="1920"
+                  :height="1516"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond-br-nossa-equipe-michela-desktop"
+                  :width="1920"
+                  :height="1516"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond-br-nossa-equipe-nesrim-desktop"
+                  :width="1920"
+                  :height="1621"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/equipe/nathaliabond-br-nossa-equipe-rebecca-desktop"
+                  :width="1920"
+                  :height="1621"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section>
+        <div class="internal-description">
+          <h3 class="internal-description-text">
+
+          </h3>
+          <p class="internal-description-text">
+
+          </p>
+        </div>
+        <div class="internal-extra">
+          <div class="internal-extra-scroll">
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/blog/nathaliabond.com.br-blog"
+                  :width="3836"
+                  :height="2056"
+                  :canExpand="true"
+                  :isVideo="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog"
+                  :width="1920"
+                  :height="7349"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-mobile"
+                  :width="414"
+                  :height="11896"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-a-aprendizagem-humana-desktop"
+                  :width="1920"
+                  :height="2863"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-a-linguagem-oral-infantil-desktop"
+                  :width="1920"
+                  :height="5212"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-a-psicoterapia-infantil-desktop"
+                  :width="1920"
+                  :height="3188"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-cuidando-dos-cuidadores-de-crianas-com-tea-desktop"
+                  :width="1920"
+                  :height="3489"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-gagueira-infantil-desktop"
+                  :width="1920"
+                  :height="2635"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-gagueira-infantil"
+                  :width="414"
+                  :height="2853"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-o-terapeuta-ocupacional-desktop"
+                  :width="1920"
+                  :height="3124"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-o-trabalho-do-profissional-de-educacao-fisica-desktop"
+                  :width="1920"
+                  :height="3051"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-por-que-a-intervencao-precoce-e-tao-importante-desktop"
+                  :width="1920"
+                  :height="4225"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-quando-procurar-um-psicopedagogo-desktop"
+                  :width="1920"
+                  :height="2975"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-quando-uma-crianca-precisa-de-fonoaudiologo-desktop"
+                  :width="1920"
+                  :height="3762"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-saiba-o-que-esperar-da-fala-da-criana-desktop"
+                  :width="1920"
+                  :height="2390"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item small">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-saiba-o-que-esperar-da-fala-da-criana"
+                  :width="414"
+                  :height="2907"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-sera-que-meu-filho-precisa-de-terapia-ocupacional-desktop"
+                  :width="1920"
+                  :height="8280"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-transtorno-espectro-do-autismo-desktop"
+                  :width="1920"
+                  :height="2360"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item small">
+                <Media
+                  src="nathaliabond/blog/nathaliabond-br-blog-transtorno-espectro-do-autismo"
+                  :width="414"
+                  :height="2602"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section>
+        <div class="internal-description">
+          <h3 class="internal-description-text">
+
+          </h3>
+          <p class="internal-description-text">
+
+          </p>
+        </div>
+        <div class="internal-extra">
+          <div class="internal-extra-scroll">
+            <div class="internal-extra-item landscape">
+                <Media
+                  src="nathaliabond/contato/nathaliabond-br-contato-desktop"
+                  :width="1920"
+                  :height="2682"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+            <div class="internal-extra-item small">
+                <Media
+                  src="nathaliabond/contato/nathaliabond-br-contato-mobile"
+                  :width="414"
+                  :height="2570"
+                  :canExpand="true"
+                  label=""/>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Related />
+    </div>
+
+    <div id="#modal" class="modal-above" v-if="modal.open">
+      <MediaExpanded
+        :source="modal.media.source"
+        :thumb="modal.media.thumb"
+        :alt="modal.media.alt"
+        :width="modal.media.width"
+        :height="modal.media.height"
+        :isVideo="modal.media.isVideo"/>
+    </div>
+  </article>
+</template>
+
+<script>
+import Media from'../../components/Media';
+import MediaExpanded from'../../components/MediaExpanded';
+import Related from'../../components/portfolio/Related';
+
+export default {
+  data() {
+    return {
+      modal: this.$store.getters.getModal
+    }
+  },
+  created() {
+    document.title = this.$route.meta.title;
+  },
+  mounted() {
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 500);
+  },
+  components: {
+    Media,
+    MediaExpanded,
+    Related
+  },
+  name: 'ClinicaDeDesenvolvimentoNathaliaBond',
+}
+</script>
+
+<style lang="scss">
+@import '../../sass/internals';
+@import '../../sass/modal';
+</style>
