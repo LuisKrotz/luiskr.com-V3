6750fed1edf637ea9cc177e17c4d5f1de918ee55
LuisKrotz <luis.krotz@gmail.com>
2022-04-23 15:34:52 -0300

    SERVER: re-create dist files to re-deploy application

---
+++ dist/precache-manifest.404d491a8213db1bb8741d5cb3cd7883.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e8c6c87aafd25628f643b83466a2b7",
+    "url": "/.htaccess"
+  },
+  {
+    "revision": "3d5fb9bbc41548326c97696c6e3dec9a",
+    "url": "/android-chrome-144x144.png"
+  },
+  {
+    "revision": "92dd91ea0acab960a83c0aed051a7976",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "f72c22e9aa8b9c8737ec71d8896f442b",
+    "url": "/android-chrome-256x256.png"
+  },
+  {
+    "revision": "1558bef342bb65fc2f299aa55b630686",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "d2df81ad65b5328b978ef56fc53d5f09",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "f33916efaeac5078e9fe",
+    "url": "/css/about.70bff873.css"
+  },
+  {
+    "revision": "ce6ffff8fea87ce89d9b",
+    "url": "/css/aboutmarco.4f0a7a49.css"
+  },
+  {
+    "revision": "087950554e09b065adae",
+    "url": "/css/app.fa618875.css"
+  },
+  {
+    "revision": "e0fcc221f54202a1a70c",
+    "url": "/css/brazilianleather.4f0a7a49.css"
+  },
+  {
+    "revision": "72c5e8d37e4562e55424",
+    "url": "/css/cecerele.4f0a7a49.css"
+  },
+  {
+    "revision": "6a67d90c717ad8d22448",
+    "url": "/css/clinicadedesenvolvimentonathaliabond.4f0a7a49.css"
+  },
+  {
+    "revision": "224f5316ad722192818d",
+    "url": "/css/coza.4f0a7a49.css"
+  },
+  {
+    "revision": "fdb238cb8d13ac03a3b0",
+    "url": "/css/gdpr.d3a5a103.css"
+  },
+  {
+    "revision": "c500ac2057be6858c947",
+    "url": "/css/melissa.4f0a7a49.css"
+  },
+  {
+    "revision": "2619fac52d9eeaea440e",
+    "url": "/css/metcha.4f0a7a49.css"
+  },
+  {
+    "revision": "d09591d2cf0685a9118e",
+    "url": "/css/minimelissa.4f0a7a49.css"
+  },
+  {
+    "revision": "ab3f007b39ff6aa92406",
+    "url": "/css/mor.4f0a7a49.css"
+  },
+  {
+    "revision": "1d97eb1e64f98bdbbeeb",
+    "url": "/css/notfound.3c83469a.css"
+  },
+  {
+    "revision": "fa0cd7963278242677f9",
+    "url": "/css/privacypolicy.d3a5a103.css"
+  },
+  {
+    "revision": "99b0c33e0757275137e6",
+    "url": "/css/sageweb.4f0a7a49.css"
+  },
+  {
+    "revision": "f542133e438e43b5b223",
+    "url": "/css/termsofuse.d3a5a103.css"
+  },
+  {
+    "revision": "d0ee67ded457159c0669",
+    "url": "/css/transa.4f0a7a49.css"
+  },
+  {
+    "revision": "77194280f8d6cdddfca7",
+    "url": "/css/vibra.4f0a7a49.css"
+  },
+  {
+    "revision": "cf0618db0ee601dc8a04fec6252663d3",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "05da880be966abaf1e8af78c5f86b4a7",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "246f9f81897beb34283570f565001622",
+    "url": "/favicon.svg"
+  },
+  {
+    "revision": "3a25d8de7fadc0e6a882188713288625",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f33916efaeac5078e9fe",
+    "url": "/js/about.8178f72c.js"
+  },
+  {
+    "revision": "ce6ffff8fea87ce89d9b",
+    "url": "/js/aboutmarco.c1a55ab1.js"
+  },
+  {
+    "revision": "087950554e09b065adae",
+    "url": "/js/app.413502e4.js"
+  },
+  {
+    "revision": "e0fcc221f54202a1a70c",
+    "url": "/js/brazilianleather.2de691be.js"
+  },
+  {
+    "revision": "72c5e8d37e4562e55424",
+    "url": "/js/cecerele.501df440.js"
+  },
+  {
+    "revision": "245d55c728424776b24a",
+    "url": "/js/chunk-vendors.60c4d9c5.js"
+  },
+  {
+    "revision": "6a67d90c717ad8d22448",
+    "url": "/js/clinicadedesenvolvimentonathaliabond.f263f3cb.js"
+  },
+  {
+    "revision": "224f5316ad722192818d",
+    "url": "/js/coza.9b90887b.js"
+  },
+  {
+    "revision": "fdb238cb8d13ac03a3b0",
+    "url": "/js/gdpr.3ca48116.js"
+  },
+  {
+    "revision": "c500ac2057be6858c947",
+    "url": "/js/melissa.7038b639.js"
+  },
+  {
+    "revision": "2619fac52d9eeaea440e",
+    "url": "/js/metcha.05d4dfde.js"
+  },
+  {
+    "revision": "d09591d2cf0685a9118e",
+    "url": "/js/minimelissa.833c94ef.js"
+  },
+  {
+    "revision": "ab3f007b39ff6aa92406",
+    "url": "/js/mor.10c9faf3.js"
+  },
+  {
+    "revision": "1d97eb1e64f98bdbbeeb",
+    "url": "/js/notfound.7feb44c9.js"
+  },
+  {
+    "revision": "fa0cd7963278242677f9",
+    "url": "/js/privacypolicy.345b1d46.js"
+  },
+  {
+    "revision": "99b0c33e0757275137e6",
+    "url": "/js/sageweb.19729383.js"
+  },
+  {
+    "revision": "f542133e438e43b5b223",
+    "url": "/js/termsofuse.3f15fa16.js"
+  },
+  {
+    "revision": "d0ee67ded457159c0669",
+    "url": "/js/transa.2354c870.js"
+  },
+  {
+    "revision": "77194280f8d6cdddfca7",
+    "url": "/js/vibra.3ece9e4f.js"
+  },
+  {
+    "revision": "3bdeaac6684d2a06ea24206810d48690",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d31c748fece42c7ce6685b58374bd4cb",
+    "url": "/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b659d07cfb7ea1a375f361101d4a9f21",
+    "url": "/share.png"
+  },
+  {
+    "revision": "9bf8326046e50a4f1f26994178f9ef25",
+    "url": "/site.webmanifest"
+  },
+  {
+    "revision": "3df8cc94cd3f0bd132634dca8d022a65",
+    "url": "/sitemap.xml"
+  },
+  {
+    "revision": "65bac7ca08715deae705991ca5a6f518",
+    "url": "/urllist.txt"
+  }
+]);
