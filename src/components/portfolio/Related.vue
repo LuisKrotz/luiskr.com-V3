ab445f974972b2969fddfd577141fa73c927c3f2
LuisKrotz <luis.krotz@gmail.com>
2022-04-26 00:03:43 -0300

    RELATED: fix component

--- src/components/portfolio/Related.vue
+++ src/components/portfolio/Related.vue
@@ -2,13 +2,13 @@
     <footer class="internal-footer" v-if="translations">
         <h2 class="internal-footer-title">{{ translations.title }}</h2>
         <div class="internal-footer-related">
-            <router-link v-for="project, key in translations.projects" class="internal-footer-related-link" :to="translations.path + project.link" :key="key">{{ project.page }}</router-link>
+            <router-link v-for="project, projectkey in translations.projects" class="internal-footer-related-link" :to="translations.path + project.link" :key="projectkey">{{ project.page }}</router-link>
         </div>
 
         <div class="internal-footer-items">
-            <template v-for="social, key in translations.socials" :key="key">
+            <template v-for="social, socialkey in translations.socials" :key="socialkey">
                 <a :href="social.link" target="_blank" class="internal-footer-items-link">{{ social.network }}</a>
-                <span v-if="n < translations.line1.length - 1" class="internal-footer-items-separator">•</span>
+                <span v-if="socialkey < translations.socials.length - 1" class="internal-footer-items-separator">•</span>
             </template>
 
             <p class="internal-footer-items-note" v-html="translations.note"></p>
